be839333fd6a57167542322e4d07d903d0b47871
Ира <89978431+Irocchka@users.noreply.github.com>
2022-09-21 12:10:45 +0300

    Add files via upload

---
+++ Program2_1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+    class Pupil
+    {
+        public virtual void Study() { }//методы
+        public virtual void Read() { }
+        public virtual void Write() { }
+        public virtual void Relax() { }
+    }
+    class ExelentPupil : Pupil//производный класс
+    {
+        public override void Study()//переопределение метода
+        {
+            Console.WriteLine("study exelent");
+        }
+        public override void Read()
+        {
+            Console.WriteLine("read exelent");
+        }
+    public override void Write()
+        {
+            Console.WriteLine("write exelent");
+        }
+    public override void Relax()
+        {
+            Console.WriteLine("relax exelent");
+        }
+
+    }
+    class GoodPupil : Pupil//производный класс
+    {
+    public override void Study()//переопределение метода
+        {
+            Console.WriteLine("study good");
+        }
+    public override void Read()
+        {
+            Console.WriteLine("read good");
+        }
+    public override void Write()
+        {
+            Console.WriteLine("write good");
+        }
+    public override void Relax()
+        {
+            Console.WriteLine("relax good");
+        }
+    }
+    class BadPupil : Pupil//производный класс
+    {
+    public override void Study()//переопределение метода
+        {
+            Console.WriteLine("study bad");
+        }
+    public override void Read()
+        {
+            Console.WriteLine("read bad");
+        }
+    public override void Write()
+        {
+            Console.WriteLine("write bad");
+        }
+    public override void Relax()
+        {
+            Console.WriteLine("relax bad");
+        }
+    }
+    class ClassRoom
+    {
+        Pupil[] pup = new Pupil[4];//массив
+        public ClassRoom(params Pupil[] _pup)//конструктор
+        {
+            for (int i = 0; i < _pup.Length; i++)
+            {
+                if (_pup != null)
+                {
+                    pup[i] = _pup[i];
+                }
+            }
+        }
+        public void Info()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (pup[i] != null)
+                {
+                    Console.WriteLine("\n" + pup[i].GetType());
+                    pup[i].Study();
+                    pup[i].Read();
+                    pup[i].Write();
+                    pup[i].Relax();
+                }
+            }
+        }
+    }
+    class ls2_1
+    {
+        static void Main(string[] args)
+        {
+        ClassRoom classRoom1 = new ClassRoom(new ExelentPupil(), new GoodPupil(), new BadPupil());
+        classRoom1.Info();
+        ClassRoom classRoom2 = new ClassRoom( new GoodPupil(), new BadPupil());
+        classRoom2.Info();
+    }
+    }
---
+++ Program2_2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Vehicle
+{
+    public int count;
+    public int speed;
+    public int year;
+    public Vehicle(int count,int speed,int year)
+    {
+        this.count = count;
+        this.speed = speed;
+        this.year = year;
+    }
+    public virtual void print()
+    {
+        Console.WriteLine($"count: {count} speed: {speed} yaer: {year} ");
+    }
+}
+
+class Plane : Vehicle
+{
+    int hight;
+    int passeger;
+    public Plane(int count, int speed, int year, int hight, int passeger) : base(count, speed, year)
+    {
+        this.hight = hight;
+        this.passeger = passeger;
+    }
+    public override void print()
+    {
+        Console.WriteLine("plane");
+        base.print();
+        Console.WriteLine($"hight: {hight} passeger: {passeger}");
+    }
+
+}
+
+class Car : Vehicle
+{
+    public Car(int count, int speed, int year) : base(count, speed, year) { }
+    public override void print()
+    {
+        Console.WriteLine("car");
+        base.print();
+    }
+
+}
+
+class Ship : Vehicle
+{
+    string port;
+    int passegers;
+    public Ship(int count, int speed, int year, string port, int passegers) : base(count, speed, year)
+    {
+        this.port = port;
+        this.passegers = passegers;
+    }
+    public override void print()
+    {
+        Console.WriteLine("ship");
+        base.print();
+        Console.WriteLine($"port: {port} passegers: {passegers}");
+    }
+
+}
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Car audi = new Car(1600000, 180, 2020);
+        audi.print();
+        Plane aero32 = new Plane(3000000, 500, 2022, 480, 160);
+        aero32.print();
+        Ship gr2 = new Ship(49209342, 380, 2009, "mew", 420);
+        gr2.print();
+        }
+    }
